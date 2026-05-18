350f9c909cbd625145dbaf7ea271144f2fb30e9d
Bittu Kumar <bittuprajapati324@gmail.com>
2026-05-18 13:22:41 +0530

    Add flask joke project

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    data = requests.get('https://official-joke-api.appspot.com/random_joke')
+
+    joke = data.json()
+
+    return render_template("index.html", joke=joke)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
